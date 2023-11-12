d1202f564ef2f4c963bcb294a209989c9d1fd838
Vijay Kumar <45945917+vijaykumar18@users.noreply.github.com>
2023-11-12 14:02:00 -0500

    Update code_review.R
    I have created a new branch for clone but you can correct me if I can clone this to R so that  can pull and push changes from R

--- code_review.R
+++ code_review.R
@@ -4,4 +4,31 @@
 ## Updated: Nov 6, 2023
 
 # script: line:
-# comment: 
+# comment: Jaime, can we add chunk of code or details for installing the PCPhelpers and pcpr packages?
+#comment:The way I installed instead of downloading them
+library(devtools)
+install_github("Columbia-PRIME/PCPhelpers") #worked fine 
+install_github("Columbia-PRIME/pcpr") #having error, also tried Lawrence suggested way but have same error 
+
+#installing *source* package ‘pcpr’ ...
+# ** using staged installation
+# ** R
+# ** byte-compile and prepare package for lazy loading
+# Error: object ‘multiprocess’ is not exported by 'namespace:future'
+# Execution halted
+# ERROR: lazy loading failed for package ‘pcpr’
+# * removing ‘/Library/Frameworks/R.framework/Versions/4.3-arm64/Resources/library/pcpr’
+# Warning message:
+#   In i.p(...) :
+#   installation of package ‘/var/folders/g8/dk9kfnvs4g126rq6lwy_pjhr0000gq/T//Rtmp9sRd3c/filedb4bbdc388a/pcpr_1.0.0.tar.gz’ had non-zero exit status
+# I tried following but looks like multipurpose package is not on R
+# Reinstall 'future' package
+install.packages("future")
+
+# Update 'multiprocess' package if available
+install.packages("multiprocess")
+
+# Then try reinstalling 'pcpr'
+remotes::install_github("Columbia-PRIME/pcpr")
+
+# Jaime; In general I have two issues so far; 1- I coudn't install pcpr package, and 2- couldn't clone this repository to R; maybe you need to grant me persmission!
