73440f3627219720c6995bbba0aaaf36882766c6
Vijay Kumar <45945917+vijaykumar18@users.noreply.github.com>
2023-11-16 14:24:48 -0500

    Update script_initiate.R
    added some comments

--- code/functions/script_initiate.R
+++ code/functions/script_initiate.R
@@ -1,5 +1,13 @@
+# script aim: This script loads required packages as well as all functions used in this study 
 # load packages for analysis
 source(paste0(packages.folder,'packages_to_load.R'))
 
 # load functions used in many scripts
 source(paste0(functions.folder,'functions.R'))
+
+# you might face issue on installing libraries like PCPhelpers and pcpr as well as "esri2sf". Please use following code to install them from github repositories
+
+# library(devtools)
+# install_github("Columbia-PRIME/PCPhelpers")
+#devtools::install_github("https://github.com/Columbia-PRIME/pcpr/tree/dev")
+#install_github("yonghah/esri2sf") 
